703e672a22f0f12755fd8d7c213182d8df7c8eaa
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-04-11 08:37:37 +0000

    Well Done

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2023_04_09_194615) do
   end
 
   create_table "post_images", force: :cascade do |t|
+    t.string "shop_name"
+    t.string "caption"
+    t.string "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
