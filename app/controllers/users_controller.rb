40594ea0ffc532f910e17cbceda84e21f2d1a582
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-04-12 13:35:53 +0000

    Ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
   
   def edit
@@ -11,13 +12,30 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to edit_user_registration_path
+    redirect_to user_path(@user.id)
+  end
+  
+  def distroy
+    @user = User.find(params[:id])
+    @user.destroy(user_params)
+    flash[:notice] = 'ユーザーを削除しました'
+    redirect_to :root
   end
 
-
   private
+  def set_user
+    @user = User.find_by(:id => params[:id])
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+  
 end
